f2f5cf740c90967fb4aa834906e8381ac221ade2
Sergey Mazilin <mazilin.sergey@yandex.ru>
2023-12-11 16:15:27 +0400

    pep8 corrections

--- backend/psyhelp/settings.py
+++ backend/psyhelp/settings.py
@@ -1,10 +1,9 @@
 import os
-
 from dotenv import load_dotenv
+from pathlib import Path
 
 load_dotenv()
 
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
