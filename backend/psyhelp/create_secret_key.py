fb4281e18299035c42494e3308b1adbef52c5fae
Mihailprogram <miha.vjk@mail.ru>
2023-12-25 01:26:29 +0500

    new update

--- backend/psyhelp/create_secret_key.py
+++ backend/psyhelp/create_secret_key.py
@@ -5,13 +5,11 @@ import string
 from chat.models import Chat
 
 
-
-def create_randomid():
+def create_randomid(lenth=10):
     later = string.ascii_letters + '0123456789'
     while True:
         id = ''
-        for i in range(10):
+        for i in range(lenth):
             id += random.choice(later)
         if not Chat.objects.filter(chat_secret_key=id).exists():
             return id
-    
