f3adcddf726f4b65d14fc0de913409ffca49660a
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-01-10 18:44:14 +0400

    почистил ненужные импорты

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,11 +1,7 @@
-from django.core.validators import MinLengthValidator
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 
-
-from django.conf import settings
-from .validators import validate_username
 from .managers import CustomUserManager
 from django.utils import timezone
 
