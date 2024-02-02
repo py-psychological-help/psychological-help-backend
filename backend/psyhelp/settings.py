0e9fb1ad477fc9b0814cb37c96782e819521e1d7
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-02-02 16:45:11 +0400

    pep8 settings

--- backend/psyhelp/settings.py
+++ backend/psyhelp/settings.py
@@ -94,7 +94,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)], # убрать в енв
+            "hosts": [('127.0.0.1', 6379)],  # убрать в енв
         },
     },
 }
