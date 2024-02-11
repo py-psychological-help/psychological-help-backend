50f54669c59265595bb29676a0fad3f8cdac9e4a
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-02-11 17:56:10 +0400

    добавил сигнал для отправки ссылки

---
+++ backend/chats/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from core.emails import (send_chat_url)
+from chats.models import Chat
+
+CustomUser = get_user_model()
+
+
+@receiver(post_save, sender=Chat)
+def sending_chat_to_client(sender, instance, created, **kwargs):
+    """Отправляет ссылку клиенту."""
+    if instance.psychologist:
+        send_chat_url(instance)
