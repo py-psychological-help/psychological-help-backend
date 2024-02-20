8fa5efab1dd7f8e327637c0f14d6c2492086c99d
Sergey Mazilin <mazilin.sergey@yandex.ru>
2024-02-20 16:44:09 +0400

    pep8

--- backend/users/models.py
+++ backend/users/models.py
@@ -134,7 +134,7 @@ class Document(models.Model):
             if (img.width > width or img.height > height
                     and settings.COMPRESS_IMAGE):
                 output_size = (width, height)
-                # Create a new resized “thumbnail” version of the image with Pillow
+                # Create a new resized “thumbnail” version of the image with P
                 img.thumbnail(output_size)
                 # Find the file name of the image
                 img_filename = Path(image.file.name).name
@@ -143,14 +143,14 @@ class Document(models.Model):
                 # Use the file extension to determine the file type from the
                 # image_types dictionary
                 img_format = image_types[img_suffix]
-                # Save the resized image into the buffer, noting the correct file
-                # type
+                # Save the resized image into the buffer, noting the correct f
+                # ile type
                 buffer = BytesIO()
                 img.save(buffer, format=img_format)
                 # Wrap the buffer in File object
                 file_object = File(buffer)
-                # Save the new resized file as usual, which will save to S3 using
-                # django-storages
+                # Save the new resized file as usual, which will save to S3
+                # using django-storages
                 image.save(img_filename, file_object)
 
     def save(self, *args, **kwargs):
